bb41cd6542cee2168a0c976d2b1959cfc98be4e3
Derrick Bray <derrickbray@yahoo.com>
2016-10-27 17:26:19 -0500

    js vue stuff

--- app/index.js
+++ app/index.js
@@ -4,37 +4,31 @@ import Vue from 'vue/dist/vue';
 const app = new Vue({
   el: '.full-page',
 
-  // data() {
-  //   return {
-  //     name: '',
-  //     data: null,
-  //     currentSelection: null,
-  //     loading: false,
-  //   };
-  // },
+  data() {
+    return {
+      results: [0],
 
+    };
+  },
 
-  // mounted() {
-  //   this.loading = true;
-  //   fetch('http://gateway.marvel.com:80/v1/public/series/9856?apikey=580e9250da79b2e0d0fc5f62790e6557')
-  //     .then((r) => r.json())
-  //     .then((data) => {
-  //       this.loading = false;
-  //       this.data = data.results;
-  //
-  //       this.chooseSeries(data.results[0]);
-  //     });
-  // },
+  mounted() {
+    fetch('http://gateway.marvel.com:80/v1/public/series/9856?apikey=580e9250da79b2e0d0fc5f62790e6557')
+        .then((r) => r.json())
+        .then((data) => {
+          this.series = data.results;
+
+          this.searchSeries(data.results[0]);
+        });
+  },
 
   methods: {
-    searchSeries(data) {
-      this.loading = true;
-      fetch('http://gateway.marvel.com/v1/public/series?limit=1&titleStartsWith=data&apikey=580e9250da79b2e0d0fc5f62790e6557')
+    searchSeries(series) {
+      fetch('http://gateway.marvel.com:80/v1/public/series/9856?apikey=580e9250da79b2e0d0fc5f62790e6557')
        .then((r) => r.json())
        .then((data) => {
-         this.loading = false;
-         this.currentSelection = data;
+         this.series = data.results;
        });
+      return app;
     },
   },
 });
