14643a628423d5b02fed7b73df75067fe7587098
Derrick Bray <derrickbray@yahoo.com>
2016-10-28 12:09:20 -0500

    added character method

--- app/index.js
+++ app/index.js
@@ -1,24 +1,35 @@
 import 'whatwg-fetch';
 import Vue from 'vue/dist/vue';
 
+const apiKey = '580e9250da79b2e0d0fc5f62790e6557';
+
 const app = new Vue({
   el: '.full-page',
 
   data() {
-    return { seriesData: null };
+    return { seriesData: null,
+            seriesId: null,
+    };
   },
 
   mounted() {
-    this.searchSeries('Spider-Man');
+    this.searchSeries('Wolverine');
   },
 
   methods: {
     searchSeries(series) {
-      fetch('http://gateway.marvel.com:80/v1/public/series/9856?apikey=580e9250da79b2e0d0fc5f62790e6557')
+      fetch(`http://gateway.marvel.com/v1/public/series?limit=1&titleStartsWith=${series}&apikey=${apiKey}`)
             .then((r) => r.json())
             .then((data) => {
               this.seriesData = data.data.results[0];
             });
+    },
+    searchCharacters(seriesId) {
+      fetch(`http://gateway.marvel.com/v1/public/series/${seriesId}/characters?apikey=${apiKey}`)
+        .then((r) => r.json())
+        .then((data) => {
+          this.seriesId = seriesData.characters.items[0];
+        });
     },
   },
 });
