e2e5f6822137f12928c8cfdb524196c20e27f434
Derrick Bray <derrickbray@yahoo.com>
2016-10-27 20:04:51 -0500

    working on rendering

--- app/index.js
+++ app/index.js
@@ -6,20 +6,20 @@ const app = new Vue({
 
   data() {
     return {
-      results: [0],
-
+      title: '',
     };
   },
 
-  mounted() {
-    fetch('http://gateway.marvel.com:80/v1/public/series/9856?apikey=580e9250da79b2e0d0fc5f62790e6557')
-        .then((r) => r.json())
-        .then((data) => {
-          this.series = data.results;
 
-          this.searchSeries(data.results[0]);
-        });
-  },
+  // mounted() {
+  //   fetch('http://gateway.marvel.com:80/v1/public/series/9856?apikey=580e9250da79b2e0d0fc5f62790e6557')
+  //       .then((r) => r.json())
+  //       .then((data) => {
+  //         this.series = data.results;
+  //
+  //         this.searchSeries(data.results[0]);
+  //       });
+  // },
 
   methods: {
     searchSeries(series) {
@@ -27,6 +27,7 @@ const app = new Vue({
        .then((r) => r.json())
        .then((data) => {
          this.series = data.results;
+         this.currentSelection = data;
        });
       return app;
     },
